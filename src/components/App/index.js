9dcaa3b48d8a12bd2b26d119e2145a14b5fe3892
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2019-12-29 17:41:53 +0200

    feat: add { user } to the App component state and update it on login / logout

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 
 import ROUTES from "../../constants/routes";
+import { withFirebase } from "../Firebase";
 import Navigation from "../Navigation";
 import SignUpPage from "../SignUpPage";
 
-export default class App extends React.Component {
+class App extends React.Component {
+  state = { user: null };
+
+  componentDidMount() {
+    this.props.firebase.auth.onAuthStateChanged(user => {
+      this.setState({ user });
+    });
+  }
+
   render() {
     return (
       <BrowserRouter>
@@ -17,3 +26,5 @@ export default class App extends React.Component {
     );
   }
 }
+
+export default withFirebase(App);
