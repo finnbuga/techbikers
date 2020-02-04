1a442bdeb256067c262a7f8157b0cad27f88e37e
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-04 23:37:43 +0000

    feat: redirect to home page on sign out

--- src/components/SignOutLink/index.js
+++ src/components/SignOutLink/index.js
@@ -1,15 +1,30 @@
-import React, { memo } from "react";
+import React from "react";
+import { withRouter } from "react-router-dom";
+
 import { withFirebase } from "../Firebase";
+import ROUTES from "../../constants/routes";
 
-const onClick = api => e => {
-  e.preventDefault();
-  api.doSignOut();
-};
+class SignOutLink extends React.PureComponent {
+  signOutAndRedirect = e => {
+    e.preventDefault();
+    this.props.firebase
+      .doSignOut()
+      .then(() => this.props.history.push(ROUTES.HOME));
+    // @todo display message to user
+    // @todo catch error
+  };
 
-const SignOutPage = memo(({ children, className, firebase }) => (
-  <a href="signout" onClick={onClick(firebase)} className={className}>
-    {children}
-  </a>
-));
+  render() {
+    return (
+      <a
+        href="signout"
+        onClick={this.signOutAndRedirect}
+        className={this.props.className}
+      >
+        {this.props.children}
+      </a>
+    );
+  }
+}
 
-export default withFirebase(SignOutPage);
+export default withRouter(withFirebase(SignOutLink));
