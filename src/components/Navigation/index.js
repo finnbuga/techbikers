68febf678d9f50eeb7fa4f86febce8c3fc2053f4
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-11 21:33:48 +0000

    refactor: wrap the Navigation component in memo()

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -6,50 +6,50 @@ import "./style.css";
 import ROUTES from "../../constants/routes";
 import SignOutLink from "../SignOutLink";
 
-const Navigation = memo(({ user }) => (
-  <Menu borderless as="nav" id="main-nav">
-    <Container>
-      <Menu.Item>
-        <Link to={ROUTES.HOME}>
-          <img
-            src="/static/img/logo@2x.png"
-            height="58"
-            width="58"
-            alt="bike"
-          />
-        </Link>
-      </Menu.Item>
-      <Menu.Item as={Link} to={ROUTES.HOME}>
-        TechBikers
-      </Menu.Item>
-      <Menu.Item as={Link} to={ROUTES.UPCOMING_RIDES}>
-        Rides
-      </Menu.Item>
-      <Menu.Item as={Link} to={ROUTES.ABOUT}>
-        About
-      </Menu.Item>
-      <Menu.Item as={Link} to={ROUTES.CHARITY}>
-        Charity
-      </Menu.Item>
-      <Menu.Item href="https://medium.com/@techbikers" target="_blank">
-        Blog
-      </Menu.Item>
-      {user ? (
-        <Menu.Item as={SignOutLink}>Sign Out</Menu.Item>
-      ) : (
-        <Menu.Item as={Link} to={ROUTES.SIGNIN}>
-          Sign In
-        </Menu.Item>
-      )}
-      <Menu text floated="right">
+export default memo(function Navigation({ user }) {
+  return (
+    <Menu borderless as="nav" id="main-nav">
+      <Container>
         <Menu.Item>
-          <Button as={Link} to={ROUTES.DONATE} color="olive" icon>
-            <Icon name="heart" /> Donate
-          </Button>
+          <Link to={ROUTES.HOME}>
+            <img
+              src="/static/img/logo@2x.png"
+              height="58"
+              width="58"
+              alt="bike"
+            />
+          </Link>
         </Menu.Item>
-      </Menu>
-    </Container>
-  </Menu>
-));
-
-export default Navigation;
+        <Menu.Item as={Link} to={ROUTES.HOME}>
+          TechBikers
+        </Menu.Item>
+        <Menu.Item as={Link} to={ROUTES.UPCOMING_RIDES}>
+          Rides
+        </Menu.Item>
+        <Menu.Item as={Link} to={ROUTES.ABOUT}>
+          About
+        </Menu.Item>
+        <Menu.Item as={Link} to={ROUTES.CHARITY}>
+          Charity
+        </Menu.Item>
+        <Menu.Item href="https://medium.com/@techbikers" target="_blank">
+          Blog
+        </Menu.Item>
+        {user ? (
+          <Menu.Item as={SignOutLink}>Sign Out</Menu.Item>
+        ) : (
+          <Menu.Item as={Link} to={ROUTES.SIGNIN}>
+            Sign In
+          </Menu.Item>
+        )}
+        <Menu text floated="right">
+          <Menu.Item>
+            <Button as={Link} to={ROUTES.DONATE} color="olive" icon>
+              <Icon name="heart" /> Donate
+            </Button>
+          </Menu.Item>
+        </Menu>
+      </Container>
+    </Menu>
+  );
+});
