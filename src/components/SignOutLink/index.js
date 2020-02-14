b5900446ad3289dbed7d0fa5e73cc077a953dc74
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-14 09:26:17 +0000

    refactor: rewrite SignOutLink into a functional component with hooks

--- src/components/SignOutLink/index.js
+++ src/components/SignOutLink/index.js
@@ -1,30 +1,23 @@
-import React from "react";
-import { withRouter } from "react-router-dom";
+import React, { useContext } from "react";
+import { useHistory } from "react-router-dom";
 
-import { withFirebase } from "../Firebase";
+import { FirebaseContext } from "../Firebase";
 import ROUTES from "../../constants/routes";
 
-class SignOutLink extends React.PureComponent {
-  signOutAndRedirect = e => {
+export default function SignOutLink({ children, ...props }) {
+  const firebase = useContext(FirebaseContext);
+  const history = useHistory();
+
+  const signOutAndRedirect = e => {
     e.preventDefault();
-    this.props.firebase
-      .doSignOut()
-      .then(() => this.props.history.push(ROUTES.HOME));
+    firebase.doSignOut().then(() => history.push(ROUTES.HOME));
     // @todo display message to user
     // @todo catch error
   };
 
-  render() {
-    return (
-      <a
-        href="signout"
-        onClick={this.signOutAndRedirect}
-        className={this.props.className}
-      >
-        {this.props.children}
-      </a>
-    );
-  }
+  return (
+    <a href={ROUTES.HOME} {...props} onClick={signOutAndRedirect}>
+      {children}
+    </a>
+  );
 }
-
-export default withRouter(withFirebase(SignOutLink));
