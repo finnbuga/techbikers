4b8074340f9b6128ef59ecbcff49bf45933178a2
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-14 09:46:01 +0000

    refactor: wrap Footer component in memo()

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,38 +2,38 @@ import React, { memo } from "react";
 
 import "./style.css";
 
-const Footer = memo(() => (
-  <footer id="footer">
-    <div className="content">
-      <p>
-        <a href="mailto:hello@techbikers.com">hello@techbikers.com</a>
-        <br />
-        4-5 Bonhill Street
-        <br />
-        London
-        <br />
-        EC2A 4BX
-      </p>
+export default memo(function Footer() {
+  return (
+    <footer id="footer">
+      <div className="content">
+        <p>
+          <a href="mailto:hello@techbikers.com">hello@techbikers.com</a>
+          <br />
+          4-5 Bonhill Street
+          <br />
+          London
+          <br />
+          EC2A 4BX
+        </p>
 
-      <p>
-        <a
-          href="http://www.facebook.com/techbikers"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Facebook
-        </a>{" "}
-        ·{" "}
-        <a
-          href="http://twitter.com/techbikers"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Twitter
-        </a>
-      </p>
-    </div>
-  </footer>
-));
-
-export default Footer;
+        <p>
+          <a
+            href="http://www.facebook.com/techbikers"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Facebook
+          </a>{" "}
+          ·{" "}
+          <a
+            href="http://twitter.com/techbikers"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Twitter
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+});
