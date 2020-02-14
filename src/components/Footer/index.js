f9d64a4532f90683c82959a00b6248c8e98c474e
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-02-14 10:54:13 +0000

    refactor: cleanup Footer by extracting subcomponents

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,35 +5,39 @@ import "./style.css";
 export default memo(function Footer() {
   return (
     <footer id="footer">
-      <div className="content">
-        <p>
-          <a href="mailto:hello@techbikers.com">hello@techbikers.com</a>
-          <br />
-          4-5 Bonhill Street
-          <br />
-          London
-          <br />
-          EC2A 4BX
-        </p>
-
-        <p>
-          <a
-            href="http://www.facebook.com/techbikers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Facebook
-          </a>{" "}
-          ·{" "}
-          <a
-            href="http://twitter.com/techbikers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Twitter
-          </a>
-        </p>
-      </div>
+      <Contact />
+      <SocialLinks />
     </footer>
   );
 });
+
+function Contact() {
+  return (
+    <p>
+      <a href="mailto:hello@techbikers.com">hello@techbikers.com</a>
+      <br />
+      4-5 Bonhill Street
+      <br />
+      London
+      <br />
+      EC2A 4BX
+    </p>
+  );
+}
+
+function SocialLinks() {
+  const links = [
+    { name: "Facebook", href: "http://www.facebook.com/techbikers" },
+    { name: "Twitter", href: "http://twitter.com/techbikers" }
+  ];
+
+  return (
+    <p id="social-links">
+      {links.map(({ name, href }, key) => (
+        <a {...{ key, href }} target="_blank" rel="noopener noreferrer">
+          {name}
+        </a>
+      ))}
+    </p>
+  );
+}
